e3aea6656199fdc6c082f1549f4a6c344aec79c1
Jeff Chan <jeffchan@Jeffs-MBP.lan>
2019-08-31 16:06:57 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,23 @@
 const cards = ["queen","queen","king","king"];
-const cardsInPlay = [];
-const cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-const cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2); {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+var cardsInPlay = [];
+
+function checkForMatch() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
+};
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+if (cardsInPlay.length === 2) {
+	checkForMatch();
 }
+}
+
+
+flipCard(3);
+flipCard(2);
+
